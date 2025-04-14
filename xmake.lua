f4625c94bd33a86a8b793d19b780b75c4ee1a41c
kylinholmes <874067181lsfy@gmail.com>
2025-04-15 03:44:55 +0800

    using xmake

---
+++ xmake.lua
@@ -0,0 +1,23 @@
+-- 设置项目名称和版本
+set_project("ringbuf_shm")
+set_version("1.0.0")
+
+
+-- 设置全局编译选项
+set_languages("cxx20") -- 使用 C++20 标准
+add_rules("mode.debug", "mode.release") -- 添加调试和发布模式规则
+set_warnings("all", "error") -- 开启所有警告并将警告视为错误
+
+-- 添加包含目录
+add_includedirs("include")
+
+-- 定义目标
+target("test_shm")
+    set_kind("binary") -- 目标类型为可执行文件
+    add_files("test/test_shm.cpp") -- 添加源文件
+    add_links("pthread") -- 链接 pthread 库
+
+-- 如果是 Linux 系统，链接 rt 库
+if is_plat("linux") then
+    add_links("rt")
+end
