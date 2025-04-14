60051a85068ac5fc3c4e531ec3d0d3485ee25f04
kylinholmes <874067181lsfy@gmail.com>
2025-04-15 04:21:09 +0800

    add ts

--- test/p1.cpp
+++ test/p1.cpp
@@ -1,13 +1,21 @@
 #include "shm_helper.hpp"
 #include "ringbuf.hpp"
+#include <cstdio>
 #include <thread>
+
+auto not_ns_ts() {
+    auto ts = std::chrono::high_resolution_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(ts).count();
+}
 
 int main() {
     using namespace std::chrono_literals;
     auto rb = ringbuf::ringbuf_t<4096, shm_helper::shm_t>("my_shm");
 
     for(auto p1=0; p1 < 1000; p1++) {
-        rb.push(p1);
+        auto ts=  not_ns_ts();
+        rb.push(ts);
+        printf("%lld\n", ts);
         std::this_thread::sleep_for(1s);
     }
 }
--- test/p2.cpp
+++ test/p2.cpp
@@ -3,19 +3,26 @@
 #include <cstdio>
 #include <thread>
 
+
+auto not_ns_ts() {
+    auto ts = std::chrono::high_resolution_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(ts).count();
+}
+
 int main() {
     using namespace std::chrono_literals;
     auto rb = ringbuf::ringbuf_t<4096, shm_helper::shm_t>("my_shm");
 
     while(true) {
-        int p2;
+        long long p2;
         auto used = rb.pop(p2);
         if(used) {
             printf("Buffer is empty, used size: %d\n", used);
-            std::this_thread::sleep_for(500ms);
+            std::this_thread::sleep_for(1s);
             continue;
         }
-        printf("%d\n", p2);
+        auto now = not_ns_ts();
+        printf("now:%lld, p1:%lld, gap:%lld\n", now, p2, now - p2);
     }
 }
 
