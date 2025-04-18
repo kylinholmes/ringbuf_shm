1660c287707c9411beefc0ef008869d99c2eb005
kylinholmes <874067181lsfy@gmail.com>
2025-04-19 00:48:01 +0800

    test

--- test/threadpool.cpp
+++ test/threadpool.cpp
@@ -20,18 +20,13 @@ public:
     }
     template <typename F, typename... Args>
     auto push(F &&f, Args &&...args) -> std::future<decltype(f(args...))> {
-        auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
-        auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
-        rb.push([task_ptr](){
-            try {
-                (*task_ptr)();
-            } catch (const std::exception& e) {
-                std::cerr << "Task exception: " << e.what() << std::endl;
-            } catch (...) {
-                std::cerr << "Task exception: unknown" << std::endl;
-            }
-        });
-        return task_ptr->get_future();
+        using return_type = decltype(f(args...));
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+        auto res = task->get_future();
+        rb.push([task]() { (*task)(); });
+        return res;
     }
     ~thread_pool_t() {
         for (auto& t : th) {
@@ -45,11 +40,12 @@ public:
 int main () {
     thread_pool_t tp(4);
     auto f1 = tp.push([](int a, int b) {
+        std::cout << "thread:" << std::this_thread::get_id() << ", a:" << a << ", b:" << b << std::endl;
         return a + b;
     }, 1, 2);
     auto f2 = tp.push([](std::string a, std::string b) {
         return a + b;
     }, "3", "4");
-    std::cout << "f1:" << f1.get() << ", f2:" << f2.get() << std::endl;
+    // std::cout << "f1:" << f1.get() << ", f2:" << f2.get() << std::endl;
 
 }
